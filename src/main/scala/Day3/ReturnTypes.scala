ca780099e568d74adaa1f5f68d6a18328d9256ab
Ezechib <blake-96@hotmail.co.uk>
2020-03-04 16:44:21 +0000

    Return Types and type interface

--- src/main/scala/Day3/ReturnTypes.scala
+++ src/main/scala/Day3/ReturnTypes.scala
@@ -3,9 +3,9 @@ package Day3
 class Word(var name: String)
 
 object ReturnTypes extends App{
-def ReturnWordType() : Unit = {
+def returnWordType() : Unit = {
   def word = new Word("World!")
   println("Hello" + word.name)
 }
-  ReturnWordType()
+  returnWordType()
 }
---
+++ src/main/scala/Day3/TypeInterface.scala
@@ -0,0 +1,7 @@
+package Day3
+
+object TypeInterface extends App {
+
+  def returnAnyInput() {println("Blake")}
+  returnAnyInput()
+}
