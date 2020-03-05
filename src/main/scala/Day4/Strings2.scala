8ecf78827c1479ec86d5a1974a99402de5d85128
Ezechib <blake-96@hotmail.co.uk>
2020-03-05 16:12:28 +0000

    Strings and Strings 2

---
+++ src/main/scala/Day4/Strings2.scala
@@ -0,0 +1,9 @@
+package Day4
+
+object StringsTwo extends App{
+  def result(word:String,name:String,char: Char,c:Char):String={
+    val res = (word+name).replace(char,c)
+    res
+  }
+  println(result("Ha","llo",'a','e'))
+}
